03ef12a6908389824a8e6fdf55a14322d4e6f426
Ant-ON <rn96>
2022-08-24 14:55:56 +0500

    Add CH9102F vid/pid to CDC driver

--- driver_cdc.c
+++ driver_cdc.c
@@ -1,7 +1,7 @@
 /*
  * libusbserial
  * 
- * Copyright (C) 2019 Anton Prozorov <prozanton@gmail.com>
+ * Copyright (C) 2019-2022 Anton Prozorov <prozanton@gmail.com>
  * Copyright (c) 2014-2015 Felix Hädicke
  * 
  * This library is free software; you can redistribute it and/or
@@ -23,28 +23,32 @@
 #include <string.h>
 #include <endian.h>
 
-#define ARDUINO_VENDOR_ID 0x2341
+#define ARDUINO_VENDOR_ID            0x2341
+#define QINHENG_VENDOR_ID            0x1A86
 
-#define CDC_DEVICE_CLASS 0x02
-#define CDC_ACM_DEVICE_SUBCLASS 0x02
+#define CDC_DEVICE_CH9102F           0x55D4
 
-#define CDC_REQTYPE_HOST2DEVICE 0x21
-#define CDC_REQTYPE_DEVICE2HOST 0xA1
+#define CDC_DEVICE_CLASS             0x02
+#define CDC_ACM_DEVICE_SUBCLASS      0x02
 
-#define CDC_REQ_SET_LINE_CODING 0x20
-#define CDC_REQ_GET_LINE_CODING 0x21
-#define CDC_REQ_SET_LINE_CONTROL 0x22
+#define CDC_REQTYPE_HOST2DEVICE      0x21
+#define CDC_REQTYPE_DEVICE2HOST      0xA1
 
-#define CDC_CONTROL_LINE_STATE_RTS 0x2
-#define CDC_CONTROL_LINE_STATE_DTR 0x1
+#define CDC_REQ_SET_LINE_CODING      0x20
+#define CDC_REQ_GET_LINE_CODING      0x21
+#define CDC_REQ_SET_LINE_CONTROL     0x22
 
-#define USB_CLASS_CDC_DATA 0x0a
+#define CDC_CONTROL_LINE_STATE_RTS   0x2
+#define CDC_CONTROL_LINE_STATE_DTR   0x1
+
+#define USB_CLASS_CDC_DATA           0x0a
 
 static const char* CDC_DEVICE_NAME_ARDUINO = "CDC Arduino";
+static const char* CDC_DEVICE_NAME_QINHENG = "CDC Qinheng";
 static const char* CDC_DEVICE_NAME_CDC_ACM = "CDC";
 
 static inline int cdc_ctrl(struct usbserial_port *port,
-		uint16_t req, uint16_t value,
+        uint16_t req, uint16_t value,
         void *data, uint16_t size)
 {
     return libusb_control_transfer(
@@ -60,16 +64,24 @@ static inline int cdc_ctrl(struct usbserial_port *port,
 
 static int cdc_set_dtr_rts(
         struct usbserial_port *port,
-		int dtr,
-		int rts)
+        int dtr,
+        int rts)
 {
     assert(port);
 
-	int control = 0;
-	if (dtr) control |= CDC_CONTROL_LINE_STATE_DTR;
-	if (rts) control |= CDC_CONTROL_LINE_STATE_RTS;
+    int control = 0;
+    if (dtr) control |= CDC_CONTROL_LINE_STATE_DTR;
+    if (rts) control |= CDC_CONTROL_LINE_STATE_RTS;
 
-	return cdc_ctrl(port, CDC_REQ_SET_LINE_CONTROL, control, NULL, 0);
+    return cdc_ctrl(port, CDC_REQ_SET_LINE_CONTROL, control, NULL, 0);
+}
+
+static int cdc_check_supported_by_vid_pid(uint16_t vid, uint16_t pid)
+{
+    if (QINHENG_VENDOR_ID == vid && CDC_DEVICE_CH9102F == pid)
+		return 1;
+
+	return 0;
 }
 
 static int cdc_check_supported_by_class(uint8_t class, uint8_t subclass)
@@ -86,6 +98,7 @@ static const char* cdc_get_device_name(uint16_t vid, uint16_t pid, uint8_t class
     switch (vid)
     {
     case ARDUINO_VENDOR_ID: return CDC_DEVICE_NAME_ARDUINO;
+    case QINHENG_VENDOR_ID: return CDC_DEVICE_NAME_QINHENG;
     default: return CDC_DEVICE_NAME_CDC_ACM;
     }
 }
@@ -100,11 +113,11 @@ static int cdc_port_init(struct usbserial_port *port)
 {
     assert(port);
 
-	int ret = usbserial_io_get_endpoint(port, USB_CLASS_CDC_DATA);
-	if (ret)
-		return ret;
+    int ret = usbserial_io_get_endpoint(port, USB_CLASS_CDC_DATA);
+    if (ret)
+        return ret;
 
-	cdc_set_dtr_rts(port, 0, 0);
+    cdc_set_dtr_rts(port, 0, 0);
 
     return 0;
 }
@@ -153,15 +166,15 @@ static int cdc_port_set_config(struct usbserial_port *port, const struct usbseri
     data[5] = parity_byte;
     data[6] = data_bits_byte;
 
-	ret = cdc_ctrl(port, CDC_REQ_SET_LINE_CODING, 0, data, sizeof(data));
+    ret = cdc_ctrl(port, CDC_REQ_SET_LINE_CODING, 0, data, sizeof(data));
     if (ret > 0)
     {
         if (ret == sizeof(data)) 
-			return 0;
-		return USBSERIAL_ERROR_CTRL_CMD_FAILED;
+            return 0;
+        return USBSERIAL_ERROR_CTRL_CMD_FAILED;
     }
-	
-	return ret;
+    
+    return ret;
 }
 
 static int cdc_start_reader(struct usbserial_port *port)
@@ -196,13 +209,13 @@ static int cdc_write(struct usbserial_port *port, const void *data, size_t size)
 static int cdc_purge(struct usbserial_port *port, int rx, int tx)
 {
     assert(port);
-	
+    
     return USBSERIAL_ERROR_UNSUPPORTED_OPERATION;
 }
 
 struct usbserial_driver driver_cdc =
 {
-    .check_supported_by_vid_pid = NULL,
+    .check_supported_by_vid_pid = cdc_check_supported_by_vid_pid,
     .check_supported_by_class = cdc_check_supported_by_class,
     .get_device_name = cdc_get_device_name,
     .get_ports_count = cdc_get_ports_count,
@@ -211,10 +224,10 @@ struct usbserial_driver driver_cdc =
     .port_set_config = cdc_port_set_config,
     .start_reader = cdc_start_reader,
     .stop_reader = cdc_stop_reader,
-	.read = cdc_read,
+    .read = cdc_read,
     .write = cdc_write,
     .purge = cdc_purge,
-	.set_dtr_rts = cdc_set_dtr_rts,
+    .set_dtr_rts = cdc_set_dtr_rts,
     .read_data_process = NULL,
 };
 
